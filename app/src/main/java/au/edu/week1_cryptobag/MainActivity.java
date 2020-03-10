edb366bc4be6d44aeda1552f0d0de2befc6392ed
maddy899 <61168821+maddy899@users.noreply.github.com>
2020-03-10 11:57:46 +1100

    Coins and recycler view

--- app/src/main/java/au/edu/week1_cryptobag/MainActivity.java
+++ app/src/main/java/au/edu/week1_cryptobag/MainActivity.java
@@ -1,14 +1,84 @@
 package au.edu.week1_cryptobag;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    public static final String EXTRA_MESSAGE = "au.edu.unsw.infs3634.beers.Message";
+    private Button mButton;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.rvList);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        CoinAdapter.RecyclerViewClickListener listener = new CoinAdapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                launchDetailActivity(position);
+            }
+        };
+
+        mAdapter = new CoinAdapter(Coin.getCoins(), listener);
+        mRecyclerView.setAdapter(mAdapter);
+
+
     }
+
+
+    private void launchDetailActivity(int position){
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, position);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(TAG, "onStartCalled");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(TAG, "onResumeCalled");
+
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(TAG, "onStopCalled");
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(TAG, "onPauseCalled");
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(TAG, "onDestroyCalled");
+    }
+
 }
